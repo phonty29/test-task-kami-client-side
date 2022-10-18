e81218485bf2216debcdec10a5008f2feecac6e2
Amedov Bekmuhamet <96624483+phonty29@users.noreply.github.com>
2022-10-18 13:07:47 +0600

    nc

--- src/rtk/api.ts
+++ src/rtk/api.ts
@@ -4,10 +4,12 @@ import { fetchItems, addItem, updateItem, deleteItem, turnPage } from './slice';
 import { productItem } from '../utils/types'; 
 import { AppDispatch } from './store';
 
+const api = "https://evening-oasis-57662.herokuapp.com";
+
 export const fetchProducts = () => {
 	return async function(dispatch: AppDispatch) {
 		try { 
-			let response = await axios.get(`/products`);
+			let response = await axios.get(`${api}/products`);
 			let productItems: productItem[] = response.data;
 			dispatch(fetchItems(productItems));  		
 			dispatch(turnPage(1));  		
@@ -19,7 +21,7 @@ export const fetchProducts = () => {
 
 export const getProduct = async (id: number | string) => {
 		try { 
-			let response = await axios.get(`/products/${id}`);
+			let response = await axios.get(`${api}/products/${id}`);
 			let productItem: productItem = response.data;
 			return productItem;
 		} catch (error) {
@@ -39,7 +41,7 @@ export const postProduct = (product: productItem) => {
 			});
 			formData.append('prices', JSON.stringify(product.prices));
 			formData.append('price', product.price);
-			const response = await axios.post(`/products`, formData, {headers: {"Content-Type": "multipart/form-data"}});
+			const response = await axios.post(`${api}/products`, formData, {headers: {"Content-Type": "multipart/form-data"}});
 			let productItem: productItem = response.data;
 			dispatch(addItem(productItem));  		
 		} catch (error) {
@@ -61,7 +63,7 @@ export const updateProduct = (product: productItem) => {
 			});
 			formData.append('prices', JSON.stringify(product.prices));
 			formData.append('price', product.price);
-			const response = await axios.put(`/products/${product._id}`, formData, {headers: {"Content-Type": "multipart/form-data"}});
+			const response = await axios.put(`${api}/products/${product._id}`, formData, {headers: {"Content-Type": "multipart/form-data"}});
 			let productItem: productItem = response.data;
 			dispatch(updateItem(productItem));  		
 		} catch (error) {
@@ -73,7 +75,7 @@ export const updateProduct = (product: productItem) => {
 export const deleteProduct = (id: string) => {
 	return async function(dispatch: AppDispatch) {
 		try { 
-			const response = await axios.delete(`/products/${id}`);
+			const response = await axios.delete(`${api}/products/${id}`);
 			let productItem: productItem = response.data;
 			dispatch(deleteItem(productItem._id));	
 			dispatch(fetchProducts());	
